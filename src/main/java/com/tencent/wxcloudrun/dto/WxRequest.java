17520e342fc218a3dc18cd7159bce560c4d9fb19
ychuanzhang <ychuanzhang@tencent.com>
2025-03-17 16:31:41 +0800

    显示定义req的字段名

--- src/main/java/com/tencent/wxcloudrun/dto/WxRequest.java
+++ src/main/java/com/tencent/wxcloudrun/dto/WxRequest.java
@@ -1,12 +1,23 @@
 package com.tencent.wxcloudrun.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
 public class WxRequest {
-    private String ToUserName;
-    private String FromUserName;
-    private Integer CreateTime;
-    private String MsgType;
-    private String MsgId;
+    @JsonProperty("ToUserName")
+    private String toUserName;
+
+    @JsonProperty("FromUserName")
+    private String fromUserName;
+
+    @JsonProperty("CreateTime")
+    private Integer createTime;
+
+    @JsonProperty("MsgType")
+    private String msgType;
+
+    @JsonProperty("MsgId")
+    private String msgId;
 }
