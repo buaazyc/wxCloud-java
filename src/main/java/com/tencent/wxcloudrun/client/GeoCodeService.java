c521620e1ff96216b283a34864e7dba409be4c67
ychuanzhang <ychuanzhang@tencent.com>
2025-03-19 12:35:36 +0800

    尝试修复geocode接口bug

--- src/main/java/com/tencent/wxcloudrun/client/GeoCodeService.java
+++ src/main/java/com/tencent/wxcloudrun/client/GeoCodeService.java
@@ -1,5 +1,6 @@
 package com.tencent.wxcloudrun.client;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -7,11 +8,8 @@ import com.tencent.wxcloudrun.model.Geocode;
 
 @Service
 public class GeoCodeService {
-    private final RestTemplate restTemplate;
-
-    public GeoCodeService(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
+    @Autowired
+    private RestTemplate restTemplate;
 
     // https://restapi.amap.com/v3/geocode/geo?address=%E5%8C%97%E4%BA%AC&key=2f19b91a99aac78221146ddd5b40f5c8
     public Geocode get(String address) {
--- src/main/java/com/tencent/wxcloudrun/model/Geocode.java
+++ src/main/java/com/tencent/wxcloudrun/model/Geocode.java
@@ -1,14 +1,20 @@
 package com.tencent.wxcloudrun.model;
 
+import java.io.Serializable;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class Geocode {
+import lombok.Data;
+
+@Data
+public class Geocode implements Serializable {
     @JsonProperty("status")
     private Integer status;
     @JsonProperty("geocodes")
     private InnerGeocode[] geocodes;
 
-    public class InnerGeocode {
+    @Data
+    public static class InnerGeocode {
         @JsonProperty("city")
         private String city;
         @JsonProperty("location")
