0361fb74eb798c9f05f8a4063f48259fc8efd27e
buaazyc <zhangyichuan.zyc@alibaba-inc.com>
2025-04-30 19:37:18 +0800

    删除缓存注解

--- src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
+++ src/main/java/com/tencent/wxcloudrun/WxCloudRunApplication.java
@@ -3,14 +3,12 @@ package com.tencent.wxcloudrun;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author zhangyichuan
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.tencent.wxcloudrun.dao"})
-@EnableCaching
 public class WxCloudRunApplication {
 
   public static void main(String[] args) {
