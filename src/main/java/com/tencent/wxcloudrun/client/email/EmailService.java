1222057b6217328202203da5a8b845a0c881330d
zhangyichuan <zhangyichuan.zyc@alibaba-inc.com>
2025-06-12 17:25:38 +0800

    feat: 优化ieg七夕文案

--- src/main/java/com/tencent/wxcloudrun/client/email/EmailService.java
+++ src/main/java/com/tencent/wxcloudrun/client/email/EmailService.java
@@ -1,6 +1,6 @@
-
 package com.tencent.wxcloudrun.client.email;
 
+import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -15,37 +15,59 @@ import org.springframework.stereotype.Component;
 @Component
 @Slf4j
 public class EmailService {
-    private static final String SMTP_HOST = "smtp.qq.com";
-    private static final String SMTP_PORT = "587";
-    private final String FROM = System.getenv("EMAIL_FROM");
-    private final String PASSWORD = System.getenv("EMAIL_PASSWORD");
+  private static final String SMTP_HOST = "smtp.qq.com";
+  private static final String SMTP_PORT = "587";
+  private final String FROM = System.getenv("EMAIL_FROM");
+  private final String PASSWORD = System.getenv("EMAIL_PASSWORD");
 
-    public void sendEmail(String to, String subject, String content) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", SMTP_HOST);
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.port", SMTP_PORT);
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.connectiontimeout", "5000");
-        props.put("mail.smtp.timeout", "5000");
-
-        Session session = Session.getInstance(props, new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(FROM, PASSWORD);
-            }
-        });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(FROM));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
-            message.setSubject(subject);
-            message.setText(content);
-            Transport.send(message);
-            log.info("Mail sent successfully!");
-        } catch (MessagingException e) {
-            log.error("Failed to send email: {}", e.getMessage());
-        }
+  public void sendEmail(String to, String subject, String content) {
+    try {
+      Message message = createMessage();
+      message.setFrom(new InternetAddress(FROM));
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+      message.setSubject(subject);
+      message.setText(content);
+      Transport.send(message);
+      log.info("Mail sent successfully!");
+    } catch (MessagingException e) {
+      log.error("Failed to send email: {}", e.getMessage());
     }
+  }
+
+  public void sendEmailForIeg(String to, String content) {
+    try {
+      Message message = createMessage();
+      String nick = javax.mail.internet.MimeUtility.encodeText("IEG读书协会");
+      message.setFrom(new InternetAddress(nick + " <" + FROM + ">"));
+      message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+      message.setSubject("IEG读书协会-七夕活动，您有新的留言！");
+      message.setText(content + "\n\n----\n邮件为IEG读书协会邮箱统一发送\n请勿直接回复该邮件，无法送达真实发件人");
+      Transport.send(message);
+      log.info("sendEmailForIeg to {} successfully!", to);
+    } catch (MessagingException e) {
+      log.error("sendEmailForIeg Failed to send email: {}", e.getMessage());
+    } catch (UnsupportedEncodingException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private Message createMessage() {
+    Properties props = new Properties();
+    props.put("mail.smtp.host", SMTP_HOST);
+    props.put("mail.smtp.auth", "true");
+    props.put("mail.smtp.port", SMTP_PORT);
+    props.put("mail.smtp.starttls.enable", "true");
+    props.put("mail.smtp.connectiontimeout", "5000");
+    props.put("mail.smtp.timeout", "5000");
+    Session session =
+        Session.getInstance(
+            props,
+            new Authenticator() {
+              @Override
+              protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(FROM, PASSWORD);
+              }
+            });
+    return new MimeMessage(session);
+  }
 }
--- src/main/java/com/tencent/wxcloudrun/controller/ieg/IegController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/ieg/IegController.java
@@ -1,13 +1,14 @@
 package com.tencent.wxcloudrun.controller.ieg;
 
 import com.tencent.wxcloudrun.client.email.EmailService;
-import com.tencent.wxcloudrun.dao.mapper.IegUserMapper;
 import com.tencent.wxcloudrun.dao.dataobject.IegUserDO;
+import com.tencent.wxcloudrun.dao.mapper.IegUserMapper;
 import com.tencent.wxcloudrun.service.provider.WxRequest;
 import com.tencent.wxcloudrun.service.provider.WxResponse;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.*;
 
 /**
@@ -31,6 +32,7 @@ public class IegController {
       return rsp;
     }
     log.info("create req={}", req);
+    MDC.put("traceid", req.getMsgId());
     rsp.setToUserName(req.getFromUserName());
     rsp.setFromUserName(req.getToUserName());
     rsp.setCreateTime(req.getCreateTime());
@@ -42,7 +44,7 @@ public class IegController {
     // 解析失败，则回复调整格式
     int contentPart = 2;
     if (parts.length != contentPart || "".equals(parts[0]) || "".equals(parts[1])) {
-      rsp.setContent("格式错误，请调整格式：第一行为收件人笔名，第二行开始为留言内容");
+      rsp.setContent("格式错误，请调整格式。\n" + helper());
       return rsp;
     }
 
@@ -50,7 +52,7 @@ public class IegController {
     IegUserDO iegUserDO = iegUserMapper.getUserByName(parts[0]);
     // 查不到收件，则回复收件人不存在
     if (iegUserDO == null || "".equals(iegUserDO.getEmail())) {
-      rsp.setContent("收件人不存在。请确保格式正确：第一行为收件人笔名，第二行开始为留言内容");
+      rsp.setContent("收件人不存在，请确保格式正确。\n" + helper());
       return rsp;
     }
     log.info("iegUserDO = {}", iegUserDO);
@@ -58,10 +60,10 @@ public class IegController {
     // 加一个本地流控
 
     // 发送邮件
-    emailService.sendEmail(iegUserDO.getEmail(), "IEG读书分享会（七夕活动）新的留言，请勿回复", parts[1]);
+    emailService.sendEmailForIeg(iegUserDO.getEmail(), parts[1]);
 
     // 回复消息发送成功
-    rsp.setContent("已成功将留言发送给" + parts[0] + "。");
+    rsp.setContent("已成功将留言发送给【" + parts[0] + "】");
     return rsp;
   }
 
@@ -71,11 +73,22 @@ public class IegController {
       return new String[] {"", ""};
     }
     // 按换行符最多分割一次
-    String[] result = input.split("\n", 2);
+    String[] result = input.split("，", 2);
     // 如果没有找到换行符，则第二部分为空
     if (result.length == 1) {
       return new String[] {result[0], ""};
     }
     return result;
+  }
+
+  private String helper() {
+    return "输入格式为：收件人笔名，留言内容。\n"
+        + "例如：张三，你书单中的《三体》我也很喜欢。\n"
+        + "注意：\n"
+        + "1. 收件人笔名必须与公布的书单列表中的完全一致；\n"
+        + "2. 请使用“中文逗号”分隔笔名和留言；\n"
+        + "3. 内容中不要包含表情，邮件无法正常展示微信表情；\n"
+        + "4. 邮件发送人为协会统一邮箱，收件人无法知道真正的收件人；\n"
+        + "5. 如果收到留言，请勿直接回复邮件。\n";
   }
 }
